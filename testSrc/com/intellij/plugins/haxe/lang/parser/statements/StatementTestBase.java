724510c2e7081e5b2af961a74580d26646998e36
Fedor Korotkov <fedor.korotkov@jetbrains.com>
2012-02-02 20:27:55 +0400

    Make abstract to avoid inspection.

--- testSrc/com/intellij/plugins/haxe/lang/parser/statements/StatementTestBase.java
+++ testSrc/com/intellij/plugins/haxe/lang/parser/statements/StatementTestBase.java
@@ -2,7 +2,7 @@ package com.intellij.plugins.haxe.lang.parser.statements;
 
 import com.intellij.plugins.haxe.lang.parser.HaxeParsingTestBase;
 
-public class StatementTestBase extends HaxeParsingTestBase {
+abstract public class StatementTestBase extends HaxeParsingTestBase {
   public StatementTestBase(String subFolder) {
     super("parsing", "haxe", "statements", subFolder);
   }
