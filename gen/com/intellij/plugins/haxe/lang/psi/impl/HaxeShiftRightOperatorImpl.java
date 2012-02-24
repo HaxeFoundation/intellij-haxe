99f3a6079edaefd499e9d182717312b1f4ce8871
Fedor Korotkov <fedor.korotkov@jetbrains.com>
2012-02-24 17:52:55 +0400

    Fix

---
+++ gen/com/intellij/plugins/haxe/lang/psi/impl/HaxeShiftRightOperatorImpl.java
@@ -0,0 +1,18 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.haxe.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.plugins.haxe.lang.lexer.HaxeTokenTypes.*;
+import com.intellij.plugins.haxe.lang.psi.*;
+
+public class HaxeShiftRightOperatorImpl extends HaxePsiCompositeElementImpl implements HaxeShiftRightOperator {
+
+  public HaxeShiftRightOperatorImpl(ASTNode node) {
+    super(node);
+  }
+
+}
