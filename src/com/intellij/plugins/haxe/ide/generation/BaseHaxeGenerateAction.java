fe8103a7e05f8f71a9b934b50f9cf32074689df7
Alexey Kudravtsev <cdr@intellij.com>
2013-04-08 17:53:34 +0400

    do not crash on no project/editor

--- src/com/intellij/plugins/haxe/ide/generation/BaseHaxeGenerateAction.java
+++ src/com/intellij/plugins/haxe/ide/generation/BaseHaxeGenerateAction.java
@@ -1,6 +1,5 @@
 package com.intellij.plugins.haxe.ide.generation;
 
-import com.intellij.lang.javascript.generation.BaseJSGenerateHandler;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -8,9 +7,7 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.plugins.haxe.lang.psi.HaxeClass;
-import com.intellij.plugins.haxe.lang.psi.HaxeFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 
@@ -39,11 +36,10 @@ public abstract class BaseHaxeGenerateAction extends AnAction {
   @Override
   public void update(final AnActionEvent e) {
     final Pair<Editor, PsiFile> editorAndPsiFile = getEditorAndPsiFile(e);
-    final VirtualFile file = e.getData(PlatformDataKeys.VIRTUAL_FILE);
     final Editor editor = editorAndPsiFile.first;
     final PsiFile psiFile = editorAndPsiFile.second;
 
-    final int caretOffset = editor.getCaretModel().getOffset();
+    final int caretOffset = editor == null ? -1 : editor.getCaretModel().getOffset();
     final boolean inClass = psiFile != null && PsiTreeUtil.getParentOfType(psiFile.findElementAt(caretOffset), HaxeClass.class) != null;
 
     e.getPresentation().setEnabled(inClass);
