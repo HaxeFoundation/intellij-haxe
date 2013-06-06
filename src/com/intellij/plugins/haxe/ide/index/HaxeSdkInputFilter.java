2d64deee25575090808326eade15b2556e1dc9c9
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-06-06 12:05:48 +0200

    introduced FileTypeSpecificInputFilter for index to expose its applicability to specific file types: allows us to have faster calculation of affected indices candidates

--- src/com/intellij/plugins/haxe/ide/index/HaxeSdkInputFilter.java
+++ src/com/intellij/plugins/haxe/ide/index/HaxeSdkInputFilter.java
@@ -2,17 +2,18 @@ package com.intellij.plugins.haxe.ide.index;
 
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.plugins.haxe.HaxeFileType;
-import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter;
 
-public class HaxeSdkInputFilter implements FileBasedIndex.InputFilter {
+public class HaxeSdkInputFilter extends DefaultFileTypeSpecificInputFilter {
   public static HaxeSdkInputFilter INSTANCE = new HaxeSdkInputFilter();
 
   private HaxeSdkInputFilter() {
+    super(HaxeFileType.HAXE_FILE_TYPE);
   }
 
   @Override
   public boolean acceptInput(VirtualFile file) {
     // ignore std stubs for different platforms
-    return file.getFileType() == HaxeFileType.HAXE_FILE_TYPE && !"_std".equals(file.getParent().getName());
+    return !"_std".equals(file.getParent().getName());
   }
 }
