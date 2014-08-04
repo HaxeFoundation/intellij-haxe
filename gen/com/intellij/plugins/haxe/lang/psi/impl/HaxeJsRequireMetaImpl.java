0a06e8bc69ebf8ae377e1a9ffb414256d821d443
AS3Boyan <denis.flash.as3@gmail.com>
2014-08-04 23:52:35 +0300

    @:jsRequire meta support

---
+++ gen/com/intellij/plugins/haxe/lang/psi/impl/HaxeJsRequireMetaImpl.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2014-2014 AS3Boyan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.haxe.lang.psi.impl;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.plugins.haxe.lang.psi.HaxeJsRequireMeta;
+import com.intellij.plugins.haxe.lang.psi.HaxeStringLiteralExpression;
+import com.intellij.plugins.haxe.lang.psi.HaxeVisitor;
+import com.intellij.psi.PsiElementVisitor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class HaxeJsRequireMetaImpl extends HaxePsiCompositeElementImpl implements HaxeJsRequireMeta {
+
+  public HaxeJsRequireMetaImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof HaxeVisitor) ((HaxeVisitor)visitor).visitJsRequireMeta(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public HaxeStringLiteralExpression getStringLiteralExpression() {
+    return findChildByClass(HaxeStringLiteralExpression.class);
+  }
+
+}
