0589b6898ebd47a90710ba631295d47897d11293
sganapavarapu1 <ganapavarapu@tivo.com>
2014-09-19 04:21:46 -0700

    Intermediate check-in, some more methods to implement in HaxePsiMethod, and AbstractHaxePsiClass. Note: Some "gen" classes changed... need to figure how to keep those changes --- ! TODO ! Pending todo is prefixed with "TODO: [TiVo]:" followed by text. Code compiles and new plugin instances runs but, did not had chance to test due to lack of menus.

---
+++ src/com/intellij/plugins/haxe/lang/psi/impl/HaxePsiField.java
@@ -0,0 +1,7 @@
+package com.intellij.plugins.haxe.lang.psi.impl;
+
+/**
+ * Created by srikanthg on 9/19/14.
+ */
+public class HaxePsiField {
+}
---
+++ src/com/intellij/plugins/haxe/lang/psi/impl/HaxePsiMethod.java
@@ -0,0 +1,7 @@
+package com.intellij.plugins.haxe.lang.psi.impl;
+
+/**
+ * Created by srikanthg on 9/18/14.
+ */
+public class HaxePsiMethod {
+}
