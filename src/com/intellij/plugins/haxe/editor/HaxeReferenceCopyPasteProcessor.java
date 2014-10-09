b3c1c656f3046058248206313938afbebd3bb902
AS3Boyan <denis.flash.as3@gmail.com>
2014-10-09 22:48:30 +0300

    https://github.com/TiVo/intellij-haxe/issues/10 Resolve references on paste

--- src/com/intellij/plugins/haxe/editor/HaxeReferenceCopyPasteProcessor.java
+++ src/com/intellij/plugins/haxe/editor/HaxeReferenceCopyPasteProcessor.java
@@ -17,21 +17,40 @@
  */
 package com.intellij.plugins.haxe.editor;
 
+import com.intellij.codeInsight.daemon.impl.CollectHighlightsUtil;
 import com.intellij.codeInsight.editorActions.CopyPastePostProcessor;
 import com.intellij.codeInsight.editorActions.TextBlockTransferableData;
+import com.intellij.openapi.application.Result;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.RangeMarker;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Ref;
+import com.intellij.plugins.haxe.ide.index.HaxeComponentIndex;
+import com.intellij.plugins.haxe.lang.psi.HaxeClass;
+import com.intellij.plugins.haxe.lang.psi.HaxeComponent;
+import com.intellij.plugins.haxe.lang.psi.HaxeReferenceExpression;
+import com.intellij.plugins.haxe.util.HaxeAddImportHelper;
+import com.intellij.plugins.haxe.util.HaxeResolveUtil;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.ArrayUtil;
 import org.jetbrains.annotations.Nullable;
 
+import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by as3boyan on 08.10.14.
  */
-public class HaxeReferenceCopyPasteProcessor implements CopyPastePostProcessor {
+public class HaxeReferenceCopyPasteProcessor implements CopyPastePostProcessor  {
+
   @Nullable
   @Override
   public TextBlockTransferableData collectTransferableData(PsiFile file, Editor editor, int[] startOffsets, int[] endOffsets) {
@@ -41,11 +60,61 @@ public class HaxeReferenceCopyPasteProcessor implements CopyPastePostProcessor {
   @Nullable
   @Override
   public TextBlockTransferableData extractTransferableData(Transferable content) {
-    return null;
+    Object transferData = null;
+    try {
+      transferData = content.getTransferData(DataFlavor.stringFlavor);
+    }
+    catch (UnsupportedFlavorException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    return new HaxeTextBlockTransferableData();
   }
 
   @Override
   public void processTransferableData(Project project, Editor editor, RangeMarker marker, int i, Ref ref, TextBlockTransferableData data) {
-    int offset = editor.getCaretModel().getOffset();
+    PsiDocumentManager.getInstance(project).commitAllDocuments();
+    final PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(editor.getDocument());
+    int[] startOffsets = new int[]{marker.getStartOffset()};
+    int[] endOffsets = new int[]{marker.getEndOffset()};
+
+    List<String> haxeClassList = new ArrayList<String>();
+    
+    for (int j = 0; j < startOffsets.length; j++) {
+      final int startOffset = startOffsets[j];
+      for (final PsiElement element : CollectHighlightsUtil.getElementsInRange(file, startOffset, endOffsets[j])) {
+        if (element instanceof HaxeReferenceExpression) {
+          HaxeReferenceExpression referenceExpression = (HaxeReferenceExpression)element;
+
+          if (referenceExpression.resolve() == null) {
+            final GlobalSearchScope scope = HaxeResolveUtil.getScopeForElement(referenceExpression);
+            final List<HaxeComponent> components =
+              HaxeComponentIndex.getItemsByName(referenceExpression.getText(), project, scope);
+            if (!components.isEmpty()) {
+              haxeClassList.add(((HaxeClass)components.get(0)).getQualifiedName());
+            }
+          }
+        }
+      }
+    }
+
+    if (haxeClassList.isEmpty()) {
+      return;
+    }
+
+    HaxeRestoreReferencesDialog dialog = new HaxeRestoreReferencesDialog(project, ArrayUtil.toStringArray(haxeClassList));
+    dialog.show();
+    String[] selectedObjects = dialog.getSelectedElements();
+
+    for (final String object : selectedObjects) {
+      new WriteCommandAction(project, file) {
+        @Override
+        protected void run(Result result) throws Throwable {
+          HaxeAddImportHelper.addImport(object, file);
+        }
+      }.execute();
+    }
   }
 }
