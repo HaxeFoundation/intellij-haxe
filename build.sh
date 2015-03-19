7be0c4c7673bb9665ee0ec0e1fb7c661a2713b73
Yannick Dominguez <yannick.dominguez@gmail.com>
2015-03-19 12:08:35 -0700

    build jar script takes IDEA version as input

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-./fetchIdea.sh
+if [[ $# -eq 0 ]] ; then
+    echo 'This script must be called with the version of IDEA to build'
+    echo 'example: ./build.sh 13.1.6'
+    exit 1
+fi
+
+./fetchIdea.sh "$1"
 
 ant -f build.xml -DIDEA_HOME=./idea-IU
