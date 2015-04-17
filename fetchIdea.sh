34a076e0a1bf501f1d165ed767ad7fcb112879d5
EBatTiVo <ebishton@tivo.com>
2015-04-16 18:51:24 -0700

    Travis test.

--- fetchIdea.sh
+++ fetchIdea.sh
@@ -28,11 +28,20 @@ mv ${ideaPath} ./idea-IU
 # Disable unneeded plugins.  Really, those that cause problems.
 # The configuration goes into the user's home directory, in to
 # a hidden directory, named for the version of Idea being run.
-echo Disabling unneeded or problematic plugins...
 idea_dir=${HOME}/.IntelliJIdea${ideaPrimaryVersionID}
 config_dir=${idea_dir}/config
 dp_file=${config_dir}/disabled_plugins.txt
+# Make the configuration directory
 [[ -d ${idea_dir} ]] || mkdir ${idea_dir}
 [[ -d ${config_dir} ]] || mkdir ${config_dir}
-echo "Git4Idea" >> ${dp_file}
+ls -alr ${HOME}
+echo Disabling unneeded or problematic plugins...
+for plugin in "Git4Idea" ; do
+    echo "        ${plugin}"
+    echo "${plugin}" >> ${dp_file}
+done
 
+echo Contents of ${dp_file}...
+cat ${dp_file}
+
+echo Finished
