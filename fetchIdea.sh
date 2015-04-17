4fb4a89e16adb30afaa467c40aec7f59927359a9
EBatTiVo <ebishton@tivo.com>
2015-04-16 23:00:03 -0700

    Revert recent change to disable plugins in fetchidea.sh. Change the version of Idea14 that we're using for test builds to 14.0.4, which has the bug with running git4idea in a headless environment fixed.

--- fetchIdea.sh
+++ fetchIdea.sh
@@ -25,14 +25,4 @@ rm -rf ideaIU-${ideaVersion}.tar.gz
 ideaPath=$(find . -name 'idea-IU*' | head -n 1)
 mv ${ideaPath} ./idea-IU
 
-# Disable unneeded plugins.  Really, those that cause problems.
-# The configuration goes into the user's home directory, in to
-# a hidden directory, named for the version of Idea being run.
-echo Disabling unneeded or problematic plugins...
-idea_dir=${HOME}/.IntelliJIdea${ideaPrimaryVersionID}
-config_dir=${idea_dir}/config
-dp_file=${config_dir}/disabled_plugins.txt
-[[ -d ${idea_dir} ]] || mkdir ${idea_dir}
-[[ -d ${config_dir} ]] || mkdir ${config_dir}
-echo "Git4Idea" >> ${dp_file}
 
