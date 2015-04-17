f35bd532053d3a52a6057df60fe3d0061fbc989b
EBatTiVo <ebishton@tivo.com>
2015-04-16 18:01:26 -0700

    Another try at Travis-ci configuration.

--- fetchIdea.sh
+++ fetchIdea.sh
@@ -7,6 +7,7 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 ideaVersion=$1
+ideaPrimaryVersionID=${ideaVersion%%\..*}
 
 # Get our IDEA dependency
 if [ -f ~/Tools/ideaIU-${ideaVersion}.tar.gz ];
@@ -25,10 +26,13 @@ ideaPath=$(find . -name 'idea-IU*' | head -n 1)
 mv ${ideaPath} ./idea-IU
 
 # Disable unneeded plugins.  Really, those that cause problems.
+# The configuration goes into the user's home directory, in to
+# a hidden directory, named for the version of Idea being run.
 echo Disabling unneeded or problematic plugins...
-echo BTW, \$HOME is ${HOME}
-config_dir=./idea-IU/config
+idea_dir=${HOME}/.IntelliJIdea${ideaPrimaryVersionID}
+config_dir=${idea_dir}/config
 dp_file=${config_dir}/disabled_plugins.txt
+[[ -d ${idea_dir} ]] || mkdir ${idea_dir}
 [[ -d ${config_dir} ]] || mkdir ${config_dir}
 echo "Git4Idea" >> ${dp_file}
 
