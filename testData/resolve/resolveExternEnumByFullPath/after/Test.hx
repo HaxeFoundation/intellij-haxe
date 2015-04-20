f3ff9f329d03c8ff2012e9b562acade92f69ddf0
as3boyan <denis.flash.as3@gmail.com>
2015-04-20 21:39:22 +0300

    Resolve Extern Enum Values from qualified name Test case for #39

---
+++ testData/resolve/resolveExternEnumByFullPath/after/Test.hx
@@ -0,0 +1,15 @@
+package ;
+
+import util.ArrayUtils;
+
+class Test {
+    static function main() {
+        function is_c(val) { return val == "c"; }
+
+        trace(ArrayUtils.delete_if([ "a", "b", "c" ], is_c));
+    }
+
+    static function test() {
+        return bar.Bar.CENTER;
+    }
+}
