011b6b25296db8ca813a19e7881c6f023e7c891a
as3boyan <denis.flash.as3@gmail.com>
2015-04-29 21:30:50 +0300

    Commit generated code

---
+++ gen/com/intellij/plugins/haxe/lang/psi/impl/HaxeExternInterfaceDeclarationImpl.java
@@ -0,0 +1,60 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.haxe.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.plugins.haxe.lang.lexer.HaxeTokenTypes.*;
+import com.intellij.plugins.haxe.lang.psi.*;
+
+public class HaxeExternInterfaceDeclarationImpl extends AbstractHaxePsiClass implements HaxeExternInterfaceDeclaration {
+
+  public HaxeExternInterfaceDeclarationImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof HaxeVisitor) ((HaxeVisitor)visitor).visitExternInterfaceDeclaration(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public HaxeComponentName getComponentName() {
+    return findChildByClass(HaxeComponentName.class);
+  }
+
+  @Override
+  @Nullable
+  public HaxeGenericParam getGenericParam() {
+    return findChildByClass(HaxeGenericParam.class);
+  }
+
+  @Override
+  @Nullable
+  public HaxeInheritList getInheritList() {
+    return findChildByClass(HaxeInheritList.class);
+  }
+
+  @Override
+  @Nullable
+  public HaxeInterfaceBody getInterfaceBody() {
+    return findChildByClass(HaxeInterfaceBody.class);
+  }
+
+  @Override
+  @Nullable
+  public HaxeMacroClassList getMacroClassList() {
+    return findChildByClass(HaxeMacroClassList.class);
+  }
+
+  @Override
+  @NotNull
+  public List<HaxePrivateKeyWord> getPrivateKeyWordList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, HaxePrivateKeyWord.class);
+  }
+
+}
