f877616f5539dd396901193d19acae5cca35da62
EBatTiVo <ebishton@tivo.com>
2015-05-15 20:35:16 -0700

    Change the recovery rule for 'new' to use expression_recovery. Fixes tivo/intellij-haxe #229 Added a unit test for the 'new' command.

---
+++ testData/parsing/haxe/expressions/New.hx
@@ -0,0 +1,42 @@
+/* Tests parsing 'new' in a ternary expression */
+
+package test_tivo_229;
+
+class A {
+    public function new() {}
+}
+
+class B extends A {
+    public function new() { super(); }
+}
+
+class C extends B {
+    public function new( a:A ) { }
+}
+
+class test {
+    public function getA(want_a:Boolean):A {
+        var qqq = (want_a == true) ? new A() : new B();
+        return qqq;
+    }
+
+    public function getB(want_a:Boolean):A {
+        var qqq = null != new B() ? new A() : new B();
+        return qqq;
+    }
+
+    public function getC(want_a:Boolean):A {
+        var qqq = null != new B() ? want_a == true ? new A() : new B() : want_a == false ? new A() : new B();
+        return qqq;
+    }
+
+    public function compareNew() : Boolean {
+        return new A() == new B();
+    }
+
+    public function getChain() : C {
+        return new C( new C( new C( new A() )));
+    }
+
+}
+
