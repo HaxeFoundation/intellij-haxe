71643c0da6a341633b70eef9e42cc801890e2e07
Ilya Kuzmichev <ilyak@playtika.com>
2016-02-01 09:53:18 +0300

    hxml missing files

---
+++ gen/com/intellij/plugins/haxe/hxml/psi/impl/HXMLHxmlImpl.java
@@ -0,0 +1,25 @@
+// This is a generated file. Not intended for manual editing.
+package com.intellij.plugins.haxe.hxml.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.intellij.plugins.haxe.hxml.psi.HXMLTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.plugins.haxe.hxml.psi.*;
+
+public class HXMLHxmlImpl extends ASTWrapperPsiElement implements HXMLHxml {
+
+  public HXMLHxmlImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof HXMLVisitor) ((HXMLVisitor)visitor).visitHxml(this);
+    else super.accept(visitor);
+  }
+
+}
