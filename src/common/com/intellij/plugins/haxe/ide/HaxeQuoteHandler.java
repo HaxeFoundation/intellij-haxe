c9db12b4c1531daf9e8bd880ecdca590bfb02f2e
winmain <d.winmain@gmail.com>
2016-11-02 17:44:37 +0700

    Fix doubling quotes when closing string literal

--- src/common/com/intellij/plugins/haxe/ide/HaxeQuoteHandler.java
+++ src/common/com/intellij/plugins/haxe/ide/HaxeQuoteHandler.java
@@ -18,15 +18,13 @@
 package com.intellij.plugins.haxe.ide;
 
 import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
-import com.intellij.plugins.haxe.lang.lexer.HaxeTokenTypeSets;
-import com.intellij.psi.TokenType;
-import com.intellij.util.ArrayUtil;
+import com.intellij.plugins.haxe.lang.lexer.HaxeTokenTypes;
 
 /**
  * @author fedor.korotkov
  */
 public class HaxeQuoteHandler extends SimpleTokenSetQuoteHandler {
   public HaxeQuoteHandler() {
-    super(ArrayUtil.append(HaxeTokenTypeSets.STRINGS.getTypes(), TokenType.BAD_CHARACTER));
+    super(HaxeTokenTypes.OPEN_QUOTE);
   }
 }
