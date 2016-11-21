8cd4fb622e6c8a67f9e29a62dc5a3330188bf4d5
winmain <d.winmain@gmail.com>
2016-11-21 15:58:42 +0700

    Use short names when generating haxe parser

--- gen/com/intellij/plugins/haxe/hxml/HXMLParser.java
+++ gen/com/intellij/plugins/haxe/hxml/HXMLParser.java
@@ -19,201 +19,201 @@
 // This is a generated file. Not intended for manual editing.
 package com.intellij.plugins.haxe.hxml;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiBuilder.Marker;
-import static com.intellij.plugins.haxe.hxml.psi.HXMLTypes.*;
-import static com.intellij.lang.parser.GeneratedParserUtilBase.*;
-import com.intellij.psi.tree.IElementType;
-import com.intellij.lang.ASTNode;
-import com.intellij.psi.tree.TokenSet;
 import com.intellij.lang.PsiParser;
+import com.intellij.psi.tree.IElementType;
+
+import static com.intellij.lang.parser.GeneratedParserUtilBase.*;
+import static com.intellij.plugins.haxe.hxml.psi.HXMLTypes.*;
 
 @SuppressWarnings({"SimplifiableIfStatement", "UnusedAssignment"})
 public class HXMLParser implements PsiParser {
 
-  public ASTNode parse(IElementType root_, PsiBuilder builder_) {
-    parseLight(root_, builder_);
-    return builder_.getTreeBuilt();
+  public ASTNode parse(IElementType t, PsiBuilder b) {
+    parseLight(t, b);
+    return b.getTreeBuilt();
   }
 
-  public void parseLight(IElementType root_, PsiBuilder builder_) {
-    boolean result_;
-    builder_ = adapt_builder_(root_, builder_, this, null);
-    Marker marker_ = enter_section_(builder_, 0, _COLLAPSE_, null);
-    if (root_ == CLASSPATH) {
-      result_ = classpath(builder_, 0);
+  public void parseLight(IElementType t, PsiBuilder b) {
+    boolean r;
+    b = adapt_builder_(t, b, this, null);
+    Marker m = enter_section_(b, 0, _COLLAPSE_, null);
+    if (t == CLASSPATH) {
+      r = classpath(b, 0);
     }
-    else if (root_ == DEFINE) {
-      result_ = define(builder_, 0);
+    else if (t == DEFINE) {
+      r = define(b, 0);
     }
-    else if (root_ == HXML) {
-      result_ = hxml(builder_, 0);
+    else if (t == HXML) {
+      r = hxml(b, 0);
     }
-    else if (root_ == LIB) {
-      result_ = lib(builder_, 0);
+    else if (t == LIB) {
+      r = lib(b, 0);
     }
-    else if (root_ == MAIN) {
-      result_ = main(builder_, 0);
+    else if (t == MAIN) {
+      r = main(b, 0);
     }
-    else if (root_ == PROPERTY) {
-      result_ = property(builder_, 0);
+    else if (t == PROPERTY) {
+      r = property(b, 0);
     }
-    else if (root_ == QUALIFIED_NAME) {
-      result_ = qualifiedName(builder_, 0);
+    else if (t == QUALIFIED_NAME) {
+      r = qualifiedName(b, 0);
     }
     else {
-      result_ = parse_root_(root_, builder_, 0);
+      r = parse_root_(t, b, 0);
     }
-    exit_section_(builder_, 0, marker_, root_, result_, true, TRUE_CONDITION);
+    exit_section_(b, 0, m, t, r, true, TRUE_CONDITION);
   }
 
-  protected boolean parse_root_(IElementType root_, PsiBuilder builder_, int level_) {
-    return simpleFile(builder_, level_ + 1);
+  protected boolean parse_root_(IElementType t, PsiBuilder b, int l) {
+    return simpleFile(b, l + 1);
   }
 
   /* ********************************************************** */
   // '-cp' SEPARATOR VALUE
-  public static boolean classpath(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "classpath")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_, level_, _NONE_, "<classpath>");
-    result_ = consumeToken(builder_, "-cp");
-    result_ = result_ && consumeTokens(builder_, 0, SEPARATOR, VALUE);
-    exit_section_(builder_, level_, marker_, CLASSPATH, result_, false, null);
-    return result_;
+  public static boolean classpath(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "classpath")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, "<classpath>");
+    r = consumeToken(b, "-cp");
+    r = r && consumeTokens(b, 0, SEPARATOR, VALUE);
+    exit_section_(b, l, m, CLASSPATH, r, false, null);
+    return r;
   }
 
   /* ********************************************************** */
   // '-D' SEPARATOR VALUE
-  public static boolean define(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "define")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_, level_, _NONE_, "<define>");
-    result_ = consumeToken(builder_, "-D");
-    result_ = result_ && consumeTokens(builder_, 0, SEPARATOR, VALUE);
-    exit_section_(builder_, level_, marker_, DEFINE, result_, false, null);
-    return result_;
+  public static boolean define(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "define")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, "<define>");
+    r = consumeToken(b, "-D");
+    r = r && consumeTokens(b, 0, SEPARATOR, VALUE);
+    exit_section_(b, l, m, DEFINE, r, false, null);
+    return r;
   }
 
   /* ********************************************************** */
   // HXML_FILE
-  public static boolean hxml(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "hxml")) return false;
-    if (!nextTokenIs(builder_, HXML_FILE)) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = consumeToken(builder_, HXML_FILE);
-    exit_section_(builder_, marker_, HXML, result_);
-    return result_;
+  public static boolean hxml(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "hxml")) return false;
+    if (!nextTokenIs(b, HXML_FILE)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, HXML_FILE);
+    exit_section_(b, m, HXML, r);
+    return r;
   }
 
   /* ********************************************************** */
   // hxml | lib | define | classpath | main | property | qualifiedName | COMMENT | CRLF
-  static boolean item_(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "item_")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = hxml(builder_, level_ + 1);
-    if (!result_) result_ = lib(builder_, level_ + 1);
-    if (!result_) result_ = define(builder_, level_ + 1);
-    if (!result_) result_ = classpath(builder_, level_ + 1);
-    if (!result_) result_ = main(builder_, level_ + 1);
-    if (!result_) result_ = property(builder_, level_ + 1);
-    if (!result_) result_ = qualifiedName(builder_, level_ + 1);
-    if (!result_) result_ = consumeToken(builder_, COMMENT);
-    if (!result_) result_ = consumeToken(builder_, CRLF);
-    exit_section_(builder_, marker_, null, result_);
-    return result_;
+  static boolean item_(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "item_")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = hxml(b, l + 1);
+    if (!r) r = lib(b, l + 1);
+    if (!r) r = define(b, l + 1);
+    if (!r) r = classpath(b, l + 1);
+    if (!r) r = main(b, l + 1);
+    if (!r) r = property(b, l + 1);
+    if (!r) r = qualifiedName(b, l + 1);
+    if (!r) r = consumeToken(b, COMMENT);
+    if (!r) r = consumeToken(b, CRLF);
+    exit_section_(b, m, null, r);
+    return r;
   }
 
   /* ********************************************************** */
   // '-lib' SEPARATOR VALUE (':' VALUE)?
-  public static boolean lib(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "lib")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_, level_, _NONE_, "<lib>");
-    result_ = consumeToken(builder_, "-lib");
-    result_ = result_ && consumeTokens(builder_, 0, SEPARATOR, VALUE);
-    result_ = result_ && lib_3(builder_, level_ + 1);
-    exit_section_(builder_, level_, marker_, LIB, result_, false, null);
-    return result_;
+  public static boolean lib(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "lib")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, "<lib>");
+    r = consumeToken(b, "-lib");
+    r = r && consumeTokens(b, 0, SEPARATOR, VALUE);
+    r = r && lib_3(b, l + 1);
+    exit_section_(b, l, m, LIB, r, false, null);
+    return r;
   }
 
   // (':' VALUE)?
-  private static boolean lib_3(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "lib_3")) return false;
-    lib_3_0(builder_, level_ + 1);
+  private static boolean lib_3(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "lib_3")) return false;
+    lib_3_0(b, l + 1);
     return true;
   }
 
   // ':' VALUE
-  private static boolean lib_3_0(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "lib_3_0")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = consumeToken(builder_, ":");
-    result_ = result_ && consumeToken(builder_, VALUE);
-    exit_section_(builder_, marker_, null, result_);
-    return result_;
+  private static boolean lib_3_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "lib_3_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, ":");
+    r = r && consumeToken(b, VALUE);
+    exit_section_(b, m, null, r);
+    return r;
   }
 
   /* ********************************************************** */
   // '-main' SEPARATOR qualifiedName
-  public static boolean main(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "main")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_, level_, _NONE_, "<main>");
-    result_ = consumeToken(builder_, "-main");
-    result_ = result_ && consumeToken(builder_, SEPARATOR);
-    result_ = result_ && qualifiedName(builder_, level_ + 1);
-    exit_section_(builder_, level_, marker_, MAIN, result_, false, null);
-    return result_;
+  public static boolean main(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "main")) return false;
+    boolean r;
+    Marker m = enter_section_(b, l, _NONE_, "<main>");
+    r = consumeToken(b, "-main");
+    r = r && consumeToken(b, SEPARATOR);
+    r = r && qualifiedName(b, l + 1);
+    exit_section_(b, l, m, MAIN, r, false, null);
+    return r;
   }
 
   /* ********************************************************** */
   // (KEY SEPARATOR VALUE) | KEY
-  public static boolean property(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "property")) return false;
-    if (!nextTokenIs(builder_, KEY)) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = property_0(builder_, level_ + 1);
-    if (!result_) result_ = consumeToken(builder_, KEY);
-    exit_section_(builder_, marker_, PROPERTY, result_);
-    return result_;
+  public static boolean property(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "property")) return false;
+    if (!nextTokenIs(b, KEY)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = property_0(b, l + 1);
+    if (!r) r = consumeToken(b, KEY);
+    exit_section_(b, m, PROPERTY, r);
+    return r;
   }
 
   // KEY SEPARATOR VALUE
-  private static boolean property_0(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "property_0")) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = consumeTokens(builder_, 0, KEY, SEPARATOR, VALUE);
-    exit_section_(builder_, marker_, null, result_);
-    return result_;
+  private static boolean property_0(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "property_0")) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeTokens(b, 0, KEY, SEPARATOR, VALUE);
+    exit_section_(b, m, null, r);
+    return r;
   }
 
   /* ********************************************************** */
   // QUALIFIEDCLASSNAME
-  public static boolean qualifiedName(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "qualifiedName")) return false;
-    if (!nextTokenIs(builder_, QUALIFIEDCLASSNAME)) return false;
-    boolean result_;
-    Marker marker_ = enter_section_(builder_);
-    result_ = consumeToken(builder_, QUALIFIEDCLASSNAME);
-    exit_section_(builder_, marker_, QUALIFIED_NAME, result_);
-    return result_;
+  public static boolean qualifiedName(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "qualifiedName")) return false;
+    if (!nextTokenIs(b, QUALIFIEDCLASSNAME)) return false;
+    boolean r;
+    Marker m = enter_section_(b);
+    r = consumeToken(b, QUALIFIEDCLASSNAME);
+    exit_section_(b, m, QUALIFIED_NAME, r);
+    return r;
   }
 
   /* ********************************************************** */
   // item_*
-  static boolean simpleFile(PsiBuilder builder_, int level_) {
-    if (!recursion_guard_(builder_, level_, "simpleFile")) return false;
-    int pos_ = current_position_(builder_);
+  static boolean simpleFile(PsiBuilder b, int l) {
+    if (!recursion_guard_(b, l, "simpleFile")) return false;
+    int c = current_position_(b);
     while (true) {
-      if (!item_(builder_, level_ + 1)) break;
-      if (!empty_element_parsed_guard_(builder_, "simpleFile", pos_)) break;
-      pos_ = current_position_(builder_);
+      if (!item_(b, l + 1)) break;
+      if (!empty_element_parsed_guard_(b, "simpleFile", c)) break;
+      c = current_position_(b);
     }
     return true;
   }
