60dab22a37a52a9bd3bad37c69aefec6e671fed4
mayakwd <loremipsum.inside@gmail.com>
2017-04-30 16:01:38 +0700

    Copyrights fix

--- src/common/com/intellij/plugins/haxe/ide/generation/GetterSetterMethodBuilder.java
+++ src/common/com/intellij/plugins/haxe/ide/generation/GetterSetterMethodBuilder.java
@@ -2,7 +2,6 @@
  * Copyright 2000-2013 JetBrains s.r.o.
  * Copyright 2014-2017 AS3Boyan
  * Copyright 2014-2014 Elias Ku
- * Copyright Fedor Korotkov
  * Copyright 2017-2017 Ilya Malanin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
--- src/common/com/intellij/plugins/haxe/ide/quickfix/CreateGetterSetterQuickfix.java
+++ src/common/com/intellij/plugins/haxe/ide/quickfix/CreateGetterSetterQuickfix.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2017 Ilya Malanin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intellij.plugins.haxe.ide.quickfix;
 
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
@@ -18,10 +33,6 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-
-/**
- * @author Ilya Malanin
- */
 
 public class CreateGetterSetterQuickfix extends BaseIntentionAction {
   private final HaxeClassModel classModel;
