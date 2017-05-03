8ae817810ccf221dddb3c325800ea14b9a1aa29d
mayakwd <loremipsum.inside@gmail.com>
2017-05-03 16:29:20 +0700

    Improvement: Convert quotes intention and interpolated strings with wrong quotes annotation

--- src/common/com/intellij/plugins/haxe/ide/annotator/HaxeSemanticAnnotator.java
+++ src/common/com/intellij/plugins/haxe/ide/annotator/HaxeSemanticAnnotator.java
@@ -24,6 +24,7 @@ import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.plugins.haxe.HaxeBundle;
+import com.intellij.plugins.haxe.ide.intention.ConvertQuotesIntention;
 import com.intellij.plugins.haxe.ide.quickfix.CreateGetterSetterQuickfix;
 import com.intellij.plugins.haxe.lang.lexer.HaxeTokenTypes;
 import com.intellij.plugins.haxe.lang.psi.*;
@@ -68,6 +69,8 @@ public class HaxeSemanticAnnotator implements Annotator {
     }
     else if (element instanceof HaxeVarDeclaration) {
       FieldChecker.check((HaxeVarDeclaration)element, holder);
+    } else if (element instanceof HaxeStringLiteralExpression) {
+      StringChecker.check((HaxeStringLiteralExpression)element, holder);
     }
   }
 }
@@ -658,5 +661,19 @@ class MethodBodyChecker {
   public static void check(HaxeMethod psi, AnnotationHolder holder) {
     final HaxeMethodModel method = psi.getModel();
     HaxeTypeResolver.getPsiElementType(method.getBodyPsi(), holder);
+  }
+}
+
+class StringChecker {
+  public static void check(HaxeStringLiteralExpression psi, AnnotationHolder holder) {
+    if (isSingleQuotesRequired(psi)) {
+      holder
+        .createWarningAnnotation(psi, "Expressions that contains string interpolation should be wrapped with single-quotes");
+    }
+  }
+
+  private static boolean isSingleQuotesRequired(HaxeStringLiteralExpression psi) {
+    return (psi.getLongTemplateEntryList().size() > 0 || psi.getShortTemplateEntryList().size() > 0) &&
+           psi.getFirstChild().textContains('"');
   }
 }
