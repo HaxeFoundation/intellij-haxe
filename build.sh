2655b091391f11755a6f373a81a72f2894dcf097
Eric Bishton <ebishton@ix.netcom.com>
2018-01-29 20:25:22 -0800

    Remove IDEA v14 and 15 support.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if [[ $# -eq 0 ]] ; then
     echo 'This script must be called with the version of IDEA to build'
-    echo 'example: ./build.sh 13.1.6'
+    echo 'example: ./build.sh 2016.3.4'
     exit 1
 fi
 
