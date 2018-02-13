9a403ee3cbcd53f51f35c33db812f65f71f80c39
mayakwd <loremipsum.inside@gmail.com>
2018-02-13 09:54:03 +0300

    Copyrights update

--- src/common/com/intellij/plugins/haxe/ide/completion/HaxeCommonCompletionPattern.java
+++ src/common/com/intellij/plugins/haxe/ide/completion/HaxeCommonCompletionPattern.java
@@ -1,5 +1,8 @@
 /*
- * Copyright 2018 Ilya Malanin
+ * Copyright 2000-2013 JetBrains s.r.o.
+ * Copyright 2014-2014 AS3Boyan
+ * Copyright 2014-2014 Elias Ku
+ * Copyright 2017-2018 Ilya Malanin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
