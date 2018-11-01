61bf9aab995097127e92675dc7232de46d45d27d
Eric Bishton <ebishton@ix.netcom.com>
2018-11-01 16:53:47 -0700

    Update expected test output.

--- testData/annotation/KnownTypeFromTypeTagOnArrayAccess.hx
+++ testData/annotation/KnownTypeFromTypeTagOnArrayAccess.hx
@@ -5,7 +5,7 @@ import <info descr="null">Array</info>;
 
 class <info descr="null">Test</info> {
   function <info descr="null">test</info>() {
-    var <info descr="null">a</info> : <info descr="null">Array</info><<info descr="null">Null<String></info>> = [ "string1", "string2" ];
+    var <info descr="null">a</info> : <info descr="null">Array</info><<info descr="null">Null</info><<info descr="null">String</info>>> = [ "string1", "string2" ];
     trace(<info descr="null">a</info>[0].<info descr="null">length</info>);
   }
 }
