f9d51f29d1559d46d9b0f8adec9e720ee25b5071
Eric Bishton <eric@bishtonsoftware.com>
2019-08-13 20:48:37 -0700

    Work around @NotNull test framework bug that was checking for an unregistered (and unneeded) InjectedLanguageManager service.  The fix was to instantiate the service for the test.

--- testSrc/com/intellij/plugins/haxe/lang/parser/HaxeParsingTestBase.java
+++ testSrc/com/intellij/plugins/haxe/lang/parser/HaxeParsingTestBase.java
@@ -2,6 +2,7 @@
  * Copyright 2000-2013 JetBrains s.r.o.
  * Copyright 2014-2014 AS3Boyan
  * Copyright 2014-2014 Elias Ku
+ * Copyright 2019 Eric Bishton
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +19,23 @@
 package com.intellij.plugins.haxe.lang.parser;
 
 import com.intellij.lang.LanguageASTFactory;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.lang.injection.MultiHostInjector;
+import com.intellij.lang.injection.MultiHostRegistrar;
+import com.intellij.mock.MockDumbService;
+import com.intellij.openapi.extensions.Extensions;
 import com.intellij.plugins.haxe.HaxeFileType;
 import com.intellij.plugins.haxe.HaxeLanguage;
+import com.intellij.plugins.haxe.lang.RegexLanguageInjector;
 import com.intellij.plugins.haxe.util.HaxeTestUtils;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageManagerImpl;
 import com.intellij.testFramework.ParsingTestCase;
+import org.assertj.core.util.Lists;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 abstract public class HaxeParsingTestBase extends ParsingTestCase {
   public HaxeParsingTestBase(String... path) {
@@ -32,6 +46,15 @@ abstract public class HaxeParsingTestBase extends ParsingTestCase {
   protected void setUp() throws Exception {
     super.setUp();
     addExplicitExtension(LanguageASTFactory.INSTANCE, HaxeLanguage.INSTANCE, new HaxeAstFactory());
+
+    // Work around @NotNull bug down in the test fixture.  Since no InjectedLanguageManager
+    // was registered, null was passed to a @NotNull function.  This affected testSimple().
+    registerExtensionPoint(Extensions.getArea(myProject), MultiHostInjector.MULTIHOST_INJECTOR_EP_NAME, MockMultiHostInjector.class);
+    registerExtensionPoint(Extensions.getArea(null), LanguageInjector.EXTENSION_POINT_NAME, RegexLanguageInjector.class); // Might as well use the real one.
+    InjectedLanguageManagerImpl manager = new InjectedLanguageManagerImpl(myProject,
+                                                                           new MockDumbService(myProject));
+    myProject.registerService(InjectedLanguageManager.class, manager);
+    // End workaround.
   }
 
   private static String getPath(String... args) {
@@ -53,5 +76,18 @@ abstract public class HaxeParsingTestBase extends ParsingTestCase {
   @Override
   protected boolean skipSpaces() {
     return true;
+  }
+
+  public static class MockMultiHostInjector implements MultiHostInjector {
+    @Override
+    public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
+
+    }
+
+    @NotNull
+    @Override
+    public List<? extends Class<? extends PsiElement>> elementsToInjectIn() {
+      return Lists.emptyList();
+    }
   }
 }
