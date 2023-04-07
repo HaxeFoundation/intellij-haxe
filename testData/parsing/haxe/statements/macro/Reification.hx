55d5b618dc99389f2f47cd1330d9ea73c3a2bd32
m0rkeulv <3193925+m0rkeulv@users.noreply.github.com>
2023-04-07 14:26:27 +0200

    adding basic support for parsing macro logic (#1099)

---
+++ testData/parsing/haxe/statements/macro/Reification.hx
@@ -0,0 +1,30 @@
+package ;
+import haxe.macro.Expr;
+
+class MacroTest {
+
+    macro public function macroArrayReification(inputArray:Expr) {
+        return ($a{ inputArray });
+    }
+
+    macro public function macroDynamicReification():Expr {
+        var dynamicType = null;
+        return macro { $v{ dynamicType } };
+    }
+
+    static public function macroBlockReification(a:Array<Expr>) {
+        return macro $b{a};
+    }
+
+    static public function macroIdentifierReification(identifier:String) {
+        return macro $i{identifier};
+    }
+
+    static public function macroFieldReification(field:Array<String>) {
+        return macro($p{field});
+    }
+
+    static public function macroExpReification(e:Expr):Dynamic {
+        return macro (function() return $e);
+    }
+}
