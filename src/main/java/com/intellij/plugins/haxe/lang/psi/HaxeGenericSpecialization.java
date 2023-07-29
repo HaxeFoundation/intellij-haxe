2a3ca666f431b30246246c23abef60007b35813d
m0rkeulv <github@m0rkeulv.net>
2023-07-29 23:50:31 +0200

    prevent stackoverflow when creating HaxeGenericSpecialization from GenericResolver and type is unknown

--- src/main/java/com/intellij/plugins/haxe/lang/psi/HaxeGenericSpecialization.java
+++ src/main/java/com/intellij/plugins/haxe/lang/psi/HaxeGenericSpecialization.java
@@ -135,7 +135,7 @@ public class HaxeGenericSpecialization implements Cloneable {
           HaxeResolveResult resolved =
             HaxeResolveResult.create((HaxeClass)context, fromGenericResolver(context, holder.getClassType().getGenericResolver()));
           specialization.put(element, name, resolved);
-        } else if (holder.getClassType() != null) {
+        } else if (holder.getClassType() != null && !holder.isUnknown()) {
           HaxeClass clazz = holder.getClassType().getHaxeClass();
           HaxeResolveResult resolved = HaxeResolveResult.create(clazz, fromGenericResolver(null, holder.getClassType().getGenericResolver()));
           specialization.put(element, name, resolved);
