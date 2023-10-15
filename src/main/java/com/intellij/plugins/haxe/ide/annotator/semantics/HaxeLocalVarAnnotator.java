ebff56e89353e5b24f430358edc79c01c8a21b87
m0rkeulv <github@m0rkeulv.net>
2023-10-15 19:37:39 +0200

    Annotate variable without tag as error when assigned to method that returns void (+ cache returntype for methods)

--- src/main/java/com/intellij/plugins/haxe/ide/annotator/semantics/HaxeLocalVarAnnotator.java
+++ src/main/java/com/intellij/plugins/haxe/ide/annotator/semantics/HaxeLocalVarAnnotator.java
@@ -2,12 +2,16 @@ package com.intellij.plugins.haxe.ide.annotator.semantics;
 
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.plugins.haxe.HaxeBundle;
 import com.intellij.plugins.haxe.lang.psi.HaxeLocalVarDeclaration;
 import com.intellij.plugins.haxe.model.HaxeLocalVarModel;
+import com.intellij.plugins.haxe.model.type.ResultHolder;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.plugins.haxe.ide.annotator.HaxeSemanticAnnotatorInspections.INCOMPATIBLE_INITIALIZATION;
+import static com.intellij.plugins.haxe.ide.annotator.semantics.HaxeSemanticsUtil.TypeTagChecker.getTypeFromVarInit;
 
 public class HaxeLocalVarAnnotator implements Annotator {
 
@@ -24,6 +28,15 @@ public class HaxeLocalVarAnnotator implements Annotator {
     HaxeLocalVarModel local = new HaxeLocalVarModel(var);
     if (local.hasInitializer() && local.hasTypeTag()) {
       HaxeSemanticsUtil.TypeTagChecker.check(local.getBasePsi(), local.getTypeTagPsi(), local.getInitializerPsi(), false, holder);
+    }else if (local.hasInitializer()) {
+      ResultHolder init = getTypeFromVarInit(local.getInitializerPsi());
+      if (init.isVoid()) {
+        // TODO bundle
+        holder.newAnnotation(HighlightSeverity.ERROR, "Variables of type Void are not allowed")
+          .range(local.getBasePsi())
+          .create();
+
+      }
     }
   }
 }
