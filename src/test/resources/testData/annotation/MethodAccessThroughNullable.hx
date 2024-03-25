245541d22b81ce3f10429ea22200f41bfb90e6d4
m0rkeulv <github@m0rkeulv.net>
2024-03-25 12:47:08 +0100

    revert minor changes to test (seems to be different behavior between intellij 2023 and 2024)

--- src/test/resources/testData/annotation/MethodAccessThroughNullable.hx
+++ src/test/resources/testData/annotation/MethodAccessThroughNullable.hx
@@ -4,7 +4,7 @@ class <info descr="null">Test</info> {
   var <info descr="null">ns</info> : <info descr="null">Null</info><<info descr="null">String</info>> = "Something";
   public function <info descr="null">new</info>() {
     trace(<info descr="null">ns</info>.<info descr="null">length</info>);
-    trace(<error descr="Int is not a callable type"><info descr="null">ns</info>.<info descr="null">length</info>()</error>);
+    trace(<error descr="Int is not a callable type"><info descr="null">ns</info>.length()</error>);
     trace(<info descr="null">ns</info>.<warning descr="Unresolved symbol">unknown</warning>());
   }
 }
