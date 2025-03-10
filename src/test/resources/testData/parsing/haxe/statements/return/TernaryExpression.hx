76849d1c181c02ee6f957f73d0a3d8a8c444c2fc
m0rkeulv <github@m0rkeulv.net>
2025-03-10 20:13:56 +0100

    Fix ternaryExpression corner-case

--- src/test/resources/testData/parsing/haxe/statements/return/TernaryExpression.hx
+++ src/test/resources/testData/parsing/haxe/statements/return/TernaryExpression.hx
@@ -1,7 +1,27 @@
 package ;
 class TestTernaryExpression {
-    public function Test() {
-        var b : Bool = true;
+    public function Test1() {
+        var b:Bool = true;
         return b ? "true" : "false";
     }
+
+    public function Test2() {
+        var s:String;
+        var b:Bool = true;
+        // important should be parsed as Ternary ( ref ? assign : assign)
+        // if the expression priority is incorrect this might end up as assign expression as the root.
+        b ? s = "true" : s = "false";
+    }
+    public function Test3() {
+        var k:Bool = true;
+        // just a sanity check to make sure also we can also parse AssignExpression with Ternary as child
+        k = true ? false : true;
+    }
+
+    public function Test4() {
+        var s:String;
+        var b:Bool = true;
+        return b ? s ?? "true" : s ?? "false";
+    }
+
 }
