589f54d4de748427ae6c494bc126abc87501653f
m0rkeulv <github@m0rkeulv.net>
2025-06-25 21:39:36 +0200

    bump build plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "2.0.21"
     // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
     // Gradle Qodana Plugin
