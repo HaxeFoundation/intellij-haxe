5d5cc24dd3ced13f1e17ddb4960d9370030a238b
m0rkeulv <github@m0rkeulv.net>
2025-07-02 15:45:40 +0200

    make console links for stacktrace file paths

--- src/main/java/com/intellij/plugins/haxe/execution/console/HaxeConsoleFilterProvider.java
+++ src/main/java/com/intellij/plugins/haxe/execution/console/HaxeConsoleFilterProvider.java
@@ -6,15 +6,33 @@ import com.intellij.execution.filters.OpenFileHyperlinkInfo;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.plugins.haxe.config.HaxeProjectSettings;
+import com.intellij.plugins.haxe.model.HaxeProjectModel;
+import com.intellij.plugins.haxe.model.HaxeSourceRootModel;
 import com.intellij.plugins.haxe.util.HaxeFileUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HaxeConsoleFilterProvider implements ConsoleFilterProvider {
 
-    public final Pattern compilerMessageWithFileAndLine = Pattern.compile("\\s+(?<label>\\w+)\\s+(?<path>(([\\w-]+)+(.[a-zA-Z]+)+)):(?<line>\\d+):\\s+(characters)\\s+(?<column>\\d+)\\-\\d+");
+    Pattern compilerMessageWithFileAndLine = Pattern.compile("(\\s*(?<label>\\w+)\\s+)?" // optional label/prefix (WARNING, ERROR etc)
+                                                             + "(?<path>((\\w:)?/)?([a-z_\\-\\s0-9.,]+(/)?)+\\.(\\w+))" // file path (note absolute path for windows expects forward slashes)
+                                                             + ":(?<line>([0-9]+))" // line
+                                                             + ":\\s+(characters)\\s+"
+                                                             + "(?<column>([0-9]+))-\\d+" // position
+                                                             + ".*", // rest of message
+            Pattern.CASE_INSENSITIVE);
+
+    Pattern stacktraceWithFileAndLine = Pattern.compile("(called\\sfrom\\s)"
+                                                        + "(?<qname>([\\w_.$]+)+)"
+                                                        + "\\s(\\("
+                                                        + "((?<path>((\\w:)?/)?([a-z_\\-\\s0-9.,]+(/)?)+\\.(\\w+)))"
+                                                        + "\\sline\\s(?<line>\\d+)"
+                                                        + "\\))",
+            Pattern.CASE_INSENSITIVE);
 
     @Override
     public Filter @NotNull [] getDefaultFilters(@NotNull Project project) {
@@ -22,21 +40,53 @@ public class HaxeConsoleFilterProvider implements ConsoleFilterProvider {
         String basePath = project.getBasePath();
         Filter psiFilter = (text, entireLength) -> {
             if(HaxeProjectSettings.getInstance(project).getDetectCodeReferencesInConsole()) {
-                Matcher matcher = compilerMessageWithFileAndLine.matcher(text);
-                if (matcher.matches()) {
-                    String path = matcher.group("path");
-                    String line = matcher.group("line");
-                    String column = matcher.group("column");
+                Matcher compilerMessageMatcher = compilerMessageWithFileAndLine.matcher(text);
+                if (compilerMessageMatcher.matches()) {
+                    String path = compilerMessageMatcher.group("path");
+                    String line = compilerMessageMatcher.group("line");
+                    String column = compilerMessageMatcher.group("column");
 
                     int lineNo = Integer.parseInt(line);
                     int columnNo = Integer.parseInt(column);
                     int offsetPath = text.indexOf(path);
 
+
                     VirtualFile virtualFile = HaxeFileUtil.locateFile(path, basePath);
+
                     if (virtualFile != null) {
                         OpenFileHyperlinkInfo openFileHyperlinkInfo = new OpenFileHyperlinkInfo(project, virtualFile, lineNo-1, columnNo-1);
                         int endOffset = offsetPath + path.length() + line.length();
                         return new Filter.Result(offsetPath, endOffset + 1, openFileHyperlinkInfo);
+                    }
+                }
+                Matcher stacktrace = stacktraceWithFileAndLine.matcher(text);
+                if (stacktrace.matches()) {
+                    String path = stacktrace.group("path");
+                    String line = stacktrace.group("line");
+
+                    int lineNo = Integer.parseInt(line);
+                    int offsetPath = text.indexOf(path);
+
+
+                    VirtualFile virtualFile = HaxeFileUtil.locateFile(path, basePath);
+                    if(virtualFile == null) {
+                        //TODO check if this slows down things
+
+                        // search all source roots for file
+                        HaxeProjectModel haxeProjectModel = HaxeProjectModel.fromProject(project);
+                        List<HaxeSourceRootModel> roots = haxeProjectModel.getRoots();
+                        virtualFile = roots.stream().map(haxeSourceRootModel -> haxeSourceRootModel.directory)
+                                .filter(Objects::nonNull)
+                                .map(directory -> directory.getVirtualFile().getUrl())
+                                .map(url -> HaxeFileUtil.locateFile(url + "/" + path))
+                                .filter(Objects::nonNull)
+                                .findFirst().orElse(null);
+                    }
+
+                    if (virtualFile != null) {
+                        OpenFileHyperlinkInfo openFileHyperlinkInfo = new OpenFileHyperlinkInfo(project, virtualFile, lineNo-1);
+                        int endOffset = offsetPath + path.length();
+                        return new Filter.Result(offsetPath, endOffset, openFileHyperlinkInfo);
                     }
                 }
             }
