1cfa8d004a0f76d6a8aca54e5c6b7eb0ab116ece
m0rkeulv <github@m0rkeulv.net>
2025-07-07 21:28:51 +0200

    bugfix: private constructors could incorrectly be marked as unused

--- src/main/java/com/intellij/plugins/haxe/ide/inspections/HaxeUnusedMethodInspection.java
+++ src/main/java/com/intellij/plugins/haxe/ide/inspections/HaxeUnusedMethodInspection.java
@@ -3,14 +3,10 @@ package com.intellij.plugins.haxe.ide.inspections;
 import com.intellij.codeInspection.*;
 import com.intellij.plugins.haxe.HaxeBundle;
 import com.intellij.plugins.haxe.ide.annotator.HaxeAnnotatingVisitor;
-import com.intellij.plugins.haxe.lang.psi.HaxeComponentName;
-import com.intellij.plugins.haxe.lang.psi.HaxeFile;
-import com.intellij.plugins.haxe.lang.psi.HaxeMethodDeclaration;
+import com.intellij.plugins.haxe.lang.psi.*;
 import com.intellij.plugins.haxe.metadata.psi.HaxeMetadataCompileTimeMeta;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReference;
+import com.intellij.plugins.haxe.model.HaxeClassModel;
+import com.intellij.psi.*;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
@@ -59,8 +55,25 @@ public class HaxeUnusedMethodInspection extends LocalInspectionTool {
                 if (methodDeclaration.isOverride()) return;
                 if (methodDeclaration.hasMetadata(KEEP, HaxeMetadataCompileTimeMeta.class)) return;
                 if (isGetterOrSetter(methodDeclaration)) return;
+                Collection<PsiReference> references;
+                //  if constructor also check new expressions
+                //  NOTE: new expressions are HaxeReference so maybe we could add them
+                //        to a reference index and avoid this complexity.
+                if(methodDeclaration.isConstructor()) {
+                    HaxeClassModel declaringClass = methodDeclaration.getModel().getDeclaringClass();
+                    if(declaringClass == null) return;
+                    SearchScope searchScope = GlobalSearchScope.projectScope(methodDeclaration.getProject());
+                    references = ReferencesSearch.search(declaringClass.haxeClass, searchScope, false)
+                            .filtering(psiReference ->
+                                    psiReference.getElement().getParent() instanceof HaxeType type
+                                                   && type.getParent() instanceof HaxeNewExpression)
+                            .findAll();
+                    // if new expression(s) found then we stop here, no need to do another search
+                    if (!references.isEmpty()) return;
+                }
+
                 SearchScope searchScope = GlobalSearchScope.projectScope(methodDeclaration.getProject());
-                Collection<PsiReference> references = ReferencesSearch.search(methodDeclaration, searchScope, false).findAll();
+                references = ReferencesSearch.search(methodDeclaration, searchScope, false).findAll();
                 if (references.isEmpty()) {
                     unusedMethodDeclarations.add(methodDeclaration);
                 }
