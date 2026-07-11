f235a31e2da776d86671d681ce53de96a9a630c9
m0rkeulv <github@m0rkeulv.net>
2026-07-11 18:30:33 +0200

    move `@:forward` completion to HaxeMetadataCompletionContributor and prevent other completion contributors from running

--- src/main/java/com/intellij/plugins/haxe/ide/completion/HaxeMetadataCompletionContributor.java
+++ src/main/java/com/intellij/plugins/haxe/ide/completion/HaxeMetadataCompletionContributor.java
@@ -1,12 +1,15 @@
 package com.intellij.plugins.haxe.ide.completion;
 
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.patterns.PsiElementPattern;
+import com.intellij.plugins.haxe.ide.lookup.HaxeLookupElement;
 import com.intellij.plugins.haxe.lang.psi.*;
 import com.intellij.plugins.haxe.model.HaxeBaseMemberModel;
+import com.intellij.plugins.haxe.util.HaxeAbstractForwardUtil;
 import com.intellij.plugins.haxe.util.HaxeResolveUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -15,14 +18,14 @@ import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
-import static com.intellij.plugins.haxe.ide.completion.HaxeCommonCompletionPattern.identifierInNewExpression;
 import static com.intellij.plugins.haxe.ide.completion.HaxeCompletionUtil.isInMetadataOfType;
 import static com.intellij.plugins.haxe.ide.completion.HaxeCompletionUtil.isInReferenceChain;
-import static com.intellij.plugins.haxe.ide.lookup.HaxeMemberLookupElement.createSynteticMember;
+import static com.intellij.plugins.haxe.lang.psi.impl.HaxeReferenceSuggestionUtil.addAbstractUnderlyingClassSuggestions;
+import static com.intellij.plugins.haxe.metadata.psi.HaxeMeta.FORWARD;
 import static com.intellij.plugins.haxe.metadata.psi.HaxeMeta.NULL_SAFETY;
 import static com.intellij.plugins.haxe.util.HaxeResolveUtil.findClassByQName;
 
@@ -46,26 +49,32 @@ public class HaxeMetadataCompletionContributor extends CompletionContributor {
              protected void addCompletions(@NotNull CompletionParameters parameters,
                                            ProcessingContext context,
                                            @NotNull CompletionResultSet result) {
-               final PsiFile file = parameters.getOriginalFile();
-               var position = parameters.getOriginalPosition();
-               boolean inChain = isInReferenceChain(parameters.getPosition());
-               boolean inMetadata = isInMetadataOfType(parameters.getPosition(), NULL_SAFETY);
-               if(inMetadata && !inChain) {
-                 position = position != null ? position : parameters.getPosition();
-                 addNullSafetyCompletion(result, file,position, position.getText());
-               }
+                 final PsiFile file = parameters.getOriginalFile();
+
+                 boolean inChain = isInReferenceChain(parameters.getPosition());
+                 if (!inChain) {
+                     boolean nullSafetyMetadata = isInMetadataOfType(parameters.getPosition(), NULL_SAFETY);
+                     if (nullSafetyMetadata) {
+                         addNullSafetyCompletion(result, file, parameters);
+                     }
+
+                     boolean forwardMetadata = isInMetadataOfType(parameters.getPosition(), FORWARD);
+                     if (forwardMetadata) {
+                         addForwardMemberCompletion(result, file,  parameters);
+                     }
+                 }
+
              }
            });
   }
 
     private static void addNullSafetyCompletion(final CompletionResultSet resultSet,
                                                 final PsiFile targetFile,
-                                                PsiElement position,
-                                                @NlsSafe String filterText) {
+                                                CompletionParameters parameters) {
         final Project project = targetFile.getProject();
         final GlobalSearchScope scope = HaxeResolveUtil.getScopeForElement(targetFile);
         final PrefixMatcher matcher = resultSet.getPrefixMatcher();
-
+        PsiElement position =  parameters.getOriginalPosition() != null ?  parameters.getOriginalPosition() : parameters.getPosition();
         HaxeClass NullSafetyMode = findClassByQName("haxe.macro.Compiler.NullSafetyMode", position);
         if (NullSafetyMode != null) {
             List<HaxeBaseMemberModel> members = NullSafetyMode.getModel().getMembersSelf();
@@ -74,6 +83,25 @@ public class HaxeMetadataCompletionContributor extends CompletionContributor {
                 resultSet.addElement(lookup);
             }
             resultSet.stopHere();
+        }
+    }
+    private static void addForwardMemberCompletion(final CompletionResultSet resultSet,
+                                                final PsiFile targetFile,
+                                                   CompletionParameters parameters) {
+        final Project project = targetFile.getProject();
+        final GlobalSearchScope scope = HaxeResolveUtil.getScopeForElement(targetFile);
+        final PrefixMatcher matcher = resultSet.getPrefixMatcher();
+
+        PsiElement position = parameters.getPosition();
+        if (HaxeAbstractForwardUtil.isElementInForwardMeta(position)) {
+            HaxeReference haxeReference = PsiTreeUtil.getParentOfType(position, HaxeReference.class);
+            if(haxeReference != null) {
+                List<HaxeLookupElement> members = new ArrayList<>();
+                addAbstractUnderlyingClassSuggestions(members, haxeReference);
+                List<LookupElement> lookupElements = members.stream().map(LookupElement.class::cast).toList();
+                resultSet.addAllElements(lookupElements);
+                resultSet.stopHere();
+            }
         }
     }
 }
