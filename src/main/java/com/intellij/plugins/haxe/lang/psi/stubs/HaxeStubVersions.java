ca199bed9cbdf42db601e35ce6971bda8469fd3d
m0rkeulv <github@m0rkeulv.net>
2026-07-26 00:32:02 +0200

    Attempt at fixing intexing errors.

--- src/main/java/com/intellij/plugins/haxe/lang/psi/stubs/HaxeStubVersions.java
+++ src/main/java/com/intellij/plugins/haxe/lang/psi/stubs/HaxeStubVersions.java
@@ -4,7 +4,7 @@ package com.intellij.plugins.haxe.lang.psi.stubs;
  * Central stub version constant. Bump this whenever any stub format changes.
  */
 public final class HaxeStubVersions {
-  public static final int STUB_VERSION = 133;
+  public static final int STUB_VERSION = 135;
 
   private HaxeStubVersions() {}
 }
